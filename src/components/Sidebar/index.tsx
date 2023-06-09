36e7d558201c2b2dbc15ea21dfb5d7b599ef52f2
Jamesson Seren de Arruda <jamesson.arruda@sga.pucminas.br>
2023-06-09 15:37:28 -0400

    Side bar completion: web and mobile

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,10 +1,57 @@
 import { Container } from "./styles"
+import { useState } from "react"
+
+import { ReactComponent as BurgerIcon} from '../../assets/burger.svg'
+import { ReactComponent as PizzaIcon} from '../../assets/pizza.svg'
+import { ReactComponent as SodaPopIcon} from '../../assets/soda.svg'
+import { ReactComponent as IceCreamIcon} from '../../assets/ice-cream.svg'
+
+import menuImg from '../../assets/menu.svg'
 
 export function SideBar(){
-  return (
-  <Container>
 
-    <h1>SideBar</h1>
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleToggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+  return (
+  <Container isMenuOpen={menuOpen}>
+    <button type="button" onClick={handleToggleMenu}>
+      <img src={menuImg} alt="Open and Close Menu" />
+    </button>
+    <nav>
+      <ul>
+        <li>
+          <a href="#">
+            <BurgerIcon />
+            <span>Hamburgers</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <PizzaIcon />
+            <span>Pizza</span>
+
+          </a>
+
+        </li>
+        <li>
+          <a href="#">
+            <SodaPopIcon />
+            <span>Soda</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <IceCreamIcon />
+            <span>Desserts</span>
+          </a>
+        </li>
+
+      </ul>
+    </nav>
+
   </Container>
   )
 }
